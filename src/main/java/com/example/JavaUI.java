ae7551feef7b66b09501b8777c5b0e552070ab51
Dawid Lebiedzki <lebiedyki@google.com>
2017-02-12 10:27:06 +0100

    first commit

---
+++ src/main/java/com/example/JavaUI.java
@@ -0,0 +1,80 @@
+package com.example;
+
+import org.jsoup.select.Evaluator.AllElements;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+@SpringUI
+@Theme("valo")
+public class JavaUI extends UI {
+
+	private VerticalLayout layout;
+
+	@Autowired
+	TodoList todoList;
+
+	@Override
+	protected void init(VaadinRequest request) {
+		setupLayout();
+		addHeader();
+		addForm();
+		addTodolist();
+		addActionButton();
+
+	}
+
+	private void setupLayout() {
+		layout = new VerticalLayout();
+		layout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		setContent(layout);
+
+	}
+
+	private void addHeader() {
+		Label header = new Label("TODO");
+		header.addStyleName(ValoTheme.LABEL_H1);
+		header.setSizeUndefined();
+		
+		layout.addComponent(header);
+	}
+
+	private void addForm() {
+		HorizontalLayout formLayout = new HorizontalLayout();
+		formLayout.setSpacing(true);
+		formLayout.setWidth("80%");
+		
+		TextField taskField = new TextField();
+		taskField.setWidth("100%");
+		Button addButton = new Button("");
+		addButton.setIcon(FontAwesome.PLUS);
+		addButton.addStyleName(ValoTheme.BUTTON_PRIMARY);
+
+		formLayout.addComponents(taskField, addButton);
+		formLayout.setExpandRatio(taskField, 1);
+		layout.addComponent(formLayout);
+	}
+
+	private void addTodolist() {
+		todoList.setWidth("80% ");
+		layout.addComponent(todoList);
+
+	}
+
+	private void addActionButton() {
+		Button deleteButton = new Button("Delete completed");
+		layout.addComponent(deleteButton);
+	}
+
+}
---
+++ src/main/java/com/example/TodoList.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+import com.vaadin.ui.VerticalLayout;
+
+@Component
+public class TodoList extends VerticalLayout{
+
+}
